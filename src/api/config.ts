76538b85ada8356172157ee660f19b6c63aa6235
Yosip Mike Colin <colinparrado@gmail.com>
2024-10-16 18:21:54 -0500

    Adding the token to the header

--- src/api/config.ts
+++ src/api/config.ts
@@ -3,3 +3,17 @@ import axios from "axios";
 export const axiosConfig = axios.create({
   baseURL: "http://localhost:5000/api",
 });
+
+axiosConfig.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token-mocion");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
